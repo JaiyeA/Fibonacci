7b15530de385df245b725640ddecc59ff246d811
Jaiye Agbonavbare <44385823+JaiyeA@users.noreply.github.com>
2020-09-10 12:19:43 -0400

    Using Recursion

--- Fibonacci.py
+++ Fibonacci.py
@@ -1,15 +1,12 @@
 #F(n) = F(n-1)+F(n-2)  where n>1
+
+#recursion
 def fibo(n):
-    if n==0:
-        print(0)
-    elif n==1:
-        print(1)
-    else:
-        f1=0
-        f2=1
-        for i in range(n-1):
-            f=f1+f2
-            f1=f2
-            f2=f
-        print(f)
-fibo(6)
+  if n==0:
+    return 0
+  elif n==1:
+    return 1
+  else:
+    return fibo(n-1)+fibo(n-2)
+
+print(fibo(3))
